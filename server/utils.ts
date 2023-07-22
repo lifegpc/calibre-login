c50c2ff567f2b74bd12a4cbb670f903a287fc238
lifegpc <g1710431395@gmail.com>
2023-07-22 09:23:00 +0800

    Bug fix

--- server/utils.ts
+++ server/utils.ts
@@ -1,5 +1,6 @@
 export function get_host(req: Request) {
     const u = new URL(req.url);
     const proto = req.headers.get("X-Forwarded-Proto");
-    return proto ? `${proto}://${u.host}` : u.origin;
+    const host = req.headers.get("X-Forwarded-Host");
+    return proto ? `${proto}://${host || u.host}` : u.origin;
 }
